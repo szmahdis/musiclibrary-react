ed8af3cf6f2074d0a2dbb31f46f8f0a7867d42ca
szmahdis <szmahdis@gmail.com>
2020-11-17 14:21:37 +0100

    add HeaderRow component and use fontface

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import SongList from './components/SongList';
+import HeaderRow from './components/HeaderRow';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <HeaderRow></HeaderRow>
       <SongList></SongList>
     </div>
   );
---
+++ my-app/src/components/HeaderRow.js
@@ -0,0 +1,16 @@
+import './HeaderRow.css'
+
+const HeaderRow = () => {
+    return(
+    <div className="header row">
+        <span></span>
+        <span></span>
+        <span>Title</span>
+        <span>Artist</span>
+        <span>Album</span>
+        <span></span>
+    </div>
+    )
+}
+
+export default HeaderRow;
