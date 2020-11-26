3aab7d017ffbbd00e39ddc98cbdc5bd8597a14ff
szmahdis <szmahdis@gmail.com>
2020-11-26 13:32:57 +0100

    clean the code

--- src/components/SongPreview.js
+++ src/components/SongPreview.js
@@ -1,77 +1,9 @@
-// import { useState, useEffect } from 'react'
 import './SongPreview.css';
 import './../font-awesome/css/all.min.css';
 
 const SongPreview = (props) => {
     const { song } = props;
-    // const [data, setData] = useState({});
-
-    // const getSongId = () => {
-    //     const url = window.location.href
-    //     const song = url.split('?')
-    //     const songId = song[1].split('=')[1]
-    //     return songId;
-    // };
-
-
-    // const getSongs = () => fetch('https://musiclibrary-server.herokuapp.com/songs')
-    //     .then((res) => res.json())
-
-    // useEffect(() => {
-    //     getSongs().then((data) => {
-    //         const songId = getSongId()
-    //         for (let item of data) {
-    //             if (item.id === songId) {
-    //                 console.log(item)
-    //                 setData(item)
-
-    //             }
-    //         }
-    //     })
-    // }, []);
-
-    // useEffect(() => {
-    //     setImage()
-    // },[]);
-
-
-    // const setImage = () => {
-    //     fetch(`https://musiclibrary-server.herokuapp.com/images/${data.cover}`)
-    //         .then((res) => {
-    //             res.json()
-    //             console.log(res.json())
-
-    //         }
-    //         )
-    // }
-
-   
-//   let audioObj = undefined
-//    const playSong = () => {
-//         if (audioObj) {
-//             audioObj.play();
-//         } else {
-//             const songId = getSongId()
-//             fetch(`https://musiclibrary-server.herokuapp.com/audio/${songId}`).then(
-//                 (response) => {
-//                     audioObj = new Audio(response.url)
-//                     audioObj.play();
-//                 }
-//             )
-//         }
-//     };
-
-//     const pauseSong = () => {
-//         if (audioObj) {
-//             audioObj.pause()
-//         }
-//     };
-// console.log(data.cover)
-
-
-// todo fix button
-//fix fetching image
-//investigate why server crashes
+  
 return (
     <div className="container">
         <img className="coverAlbum" id="coverAlbum" src={`https://musiclibrary-server.herokuapp.com/images/${song.cover}`} alt="coverAlbum" />
